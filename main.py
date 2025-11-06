5bf9cdfef52954255329a919a5390c5cd16a0250
Алишер Platform <@a.dautov>
2025-11-06 23:41:49 +0500

    Created Bot To Answer Nonsense

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello world")
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("just greeting message")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_message = update.message.text
+    await update.message.reply_text(f"Ты написал: {user_message}. Я бот и просто отвечаю!")
+
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    print("Бот запущен...")
+    app.run_polling()
